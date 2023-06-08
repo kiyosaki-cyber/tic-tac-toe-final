082ec26004903ae866d3f6103a53385bb77b0b73
kiyosaki-cyber <134641062+kiyosaki-cyber@users.noreply.github.com>
2023-06-08 17:25:34 +0300

    created function box click
    this is to enable the players to input their symbol on the grid board on clicking the cells.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 let Symbol="X"
 let End = false
 //assignment of variables
+function boxClick(cell){
+    let disp=document.getElementById("disp")
+     let cell=document.getElementById(cell)
+      if (cell.innerHTML === "" && !End) {
+        cell.innerHTML = Symbol;
+        disp.innerHTML = Symbol==="X"? "O" : "X";
+        win()
+        disp.classList.add(Symbol.toLowerCase()) 
+        cell.classList.add(Symbol.toLowerCase()) 
+        //create class of the specific symbol that can be smanipulated in CSS
+        toggle();
+        }
+        draw()
+        //calls the function for alternating change of symbols
+      };
