ec67f9f2665d6670f85a2636a1294420541021a2
Griffins2005 <134501193+Griffins2005@users.noreply.github.com>
2023-06-08 18:54:03 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-
+let Symbol="X"
+let End = false
+//assignment of variables
+function boxClick(cell){
+    let disp=document.getElementById("disp")
+     let cell=document.getElementById(cell)
+      if (cell.innerHTML === "" && !End) {
+        cell.innerHTML = Symbol;
+        disp.innerHTML = Symbol==="X"? "O" : "X";
+        win()
+        disp.classList.add(Symbol.toLowerCase()) 
+        cell.classList.add(Symbol.toLowerCase()) 
+        //create class of the specific symbol that can be smanipulated in CSS
+        toggle();
+        }
+        draw()
+        //calls the function for alternating change of symbols
+      };
+      function toggle(){
+        Symbol = Symbol === "X" ? "O" : "X";
+        
+     }
+     let winner= [
+        [1, 2, 3], [4, 5, 6], 
+        [7, 8, 9], [1, 4, 7], 
+        [2, 5, 8], [3, 6, 9], 
+        [1, 5, 9], [3, 5, 7]
+      ];
  //Set all wining positions as  sub arrays within an array
 function win(){
   for(let i=0;i<winner.length;i++){
